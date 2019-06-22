6ca47654e3faa990279b117082a00162c2300dad
KillZoom <danghuyvms@gmail.com>
2019-06-22 16:28:19 +0700

    My First Commit
    commit of video 1

---
+++ src/main/java/com/danghuy/controller/TrangChuController.java
@@ -0,0 +1,21 @@
+package com.danghuy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TrangChuController {
+    @RequestMapping("/")
+    public String viewTrangChu(){
+
+        return "trangchu";
+    }
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public String login(){
+
+        return "Login";
+    }
+}
