15cd703012f90e39d04cf507f0e3c4fa7bdd8eff
KillZoom <danghuyvms@gmail.com>
2019-07-01 19:46:40 +0700

    Updates commit 9, Ví dụ chuyển page khi xử lý đăng nhập + fix lỗi đường dẫn local lỗi bửa giờ :v

---
+++ src/main/java/com/danghuy/controller/DangNhapController.java
@@ -0,0 +1,26 @@
+package com.danghuy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("dangnhap/")
+public class DangNhapController{
+
+    @GetMapping
+    public String pageDefault(){
+
+        return "dangnhap";
+    }
+    @PostMapping
+    public String xuLyDangNhap(@RequestParam String username, @RequestParam String password){
+        if(username.equals("danghuy998") && password.equals("123456")){
+            return "redirect:/";
+        }else
+        return "dangnhap";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/dangnhap.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: OS
+  Date: 7/1/2019
+  Time: 7:15 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Page Dang Nhap</title>
+</head>
+<body>
+<form method="post">
+    <input type="text" name="username"><br/>
+    <input type="password" name="password"><br/>
+    <input type="submit">
+</form>
+</body>
+</html>
