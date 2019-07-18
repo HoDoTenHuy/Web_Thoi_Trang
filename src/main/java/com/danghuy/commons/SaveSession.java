76eff56c4c98c0608c1a40c9b4859a3226f291f4
KillZoom <danghuyvms@gmail.com>
2019-07-18 16:42:00 +0700

    Create SaveSession.java

---
+++ src/main/java/com/danghuy/commons/SaveSession.java
@@ -0,0 +1,42 @@
+package com.danghuy.commons;
+
+import com.danghuy.entity.GioHang;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Component
+public class SaveSession {
+    public void loginUser(HttpSession httpSession, ModelMap modelMap){
+        String chuCaiDau;
+        if (httpSession.getAttribute("user") != null) {
+            String email = (String) httpSession.getAttribute("user");
+            chuCaiDau = email.substring(0, 1).toUpperCase();
+        }else {
+            chuCaiDau = null;
+        }
+        modelMap.addAttribute("chuCaiDau", chuCaiDau);
+
+    }
+    public void shoppingCart(HttpSession httpSession, ModelMap modelMap){
+        int soSanPham;
+        if (httpSession.getAttribute("cart") != null) {
+            List<GioHang> gioHangs = (List<GioHang>) httpSession.getAttribute("cart");
+            soSanPham = gioHangs.size();
+        }else{
+            soSanPham = 0;
+        }
+        modelMap.addAttribute("sosanpham", soSanPham);
+    }
+    public List<GioHang> gioHangList(HttpSession httpSession){
+        List<GioHang> gioHangs = null;
+        if (httpSession.getAttribute("cart") != null) {
+            gioHangs = (List<GioHang>) httpSession.getAttribute("cart");
+            return gioHangs;
+        }
+        return gioHangs;
+    }
+}
